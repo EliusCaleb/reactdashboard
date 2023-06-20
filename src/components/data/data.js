79570b62693fdb0de3e6b3812952ca0d9d828e8e
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-06-20 18:52:38 +0300

    created the  update component   in the dashboard

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -8,6 +8,12 @@ import {
   } from "@iconscout/react-unicons";
 
 
+  import img1 from "../../assets/img1.png";
+  import img2 from "../../assets/img2.png";
+  import img3 from "../../assets/img3.png";
+  
+
+
 import { UilUsdSquare, UilMoneyWithdrawal } from "@iconscout/react-unicons";
   // Sidebar Data
 export const SidebarData = [
@@ -84,5 +90,28 @@ export const cardsData = [
         data: [10, 25, 15, 30, 12, 15, 20],
       },
     ],
+  },
+];
+
+
+// Recent Update Card Data
+export const UpdatesData = [
+  {
+    img: img1,
+    name: "Andrew Thomas",
+    noti: "has ordered Apple smart watch 2500mh battery.",
+    time: "25 seconds ago",
+  },
+  {
+    img: img2,
+    name: "James Bond",
+    noti: "has received Samsung gadget for charging battery.",
+    time: "30 minutes ago",
+  },
+  {
+    img: img3,
+    name: "Iron Man",
+    noti: "has ordered Apple smart watch, samsung Gear 2500mh battery.",
+    time: "2 hours ago",
   },
 ];
