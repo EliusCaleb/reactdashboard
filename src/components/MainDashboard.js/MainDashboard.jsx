9cf3966583ea9749c6be63af9806ecf45b779d45
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-06-20 18:15:37 +0300

    added table to main dashboard

--- src/components/MainDashboard.js/MainDashboard.jsx
+++ src/components/MainDashboard.js/MainDashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Card from '../Cards/Cards';
 import './MainDashboard.css'
+import Table from '../Table/Table'
 
 const MainDashboard = () => {
   return (
     <div className='mainDash'>
       <h1>Dashboard</h1>
       <Card/>
+      <Table/>
     </div>
   )
 }
