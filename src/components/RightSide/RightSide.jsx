916f5ca4a6b21e90a12d6224fb9e46cef9d3f3a7
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-06-20 18:51:57 +0300

    creating the right side  of  app

---
+++ src/components/RightSide/RightSide.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Updates from '../Updates/Updates';
+
+const RightSide = () => {
+  return (
+    <div className='rightside'>
+             <div className=''>
+              <h2>Updates</h2>
+               <Updates/>
+             </div>
+             <div className=''>
+              <h2>Customers Reviews</h2>
+              
+             </div>
+    </div>
+  )
+}
+
+export default RightSide
