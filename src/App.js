98e73c42a74597552461ac25c21c0b36045c6ab2
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-06-20 18:16:18 +0300

    styling the  card component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import MainDashboard from './components/MainDashboard.js/MainDashboard';
+import RightSide from './components/RightSide/RightSide';
 import Sidebar from './components/Sidebar';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
         <div className='appGlass'>
          <Sidebar/>
          <MainDashboard/>
+         <RightSide/>
 
         </div>
     </div>
